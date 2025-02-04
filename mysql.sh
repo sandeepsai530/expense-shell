a9d85ea7674ce32865bda3ef960c043b44e275f8
sai sandeep <sandeepsai530@gmail.com>
2025-02-04 16:34:12 +0530

    added mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -22,7 +22,7 @@ VALIDATE(){
 }
 
 CHECK_ROOT(){
-    if [ $1 -ne 0 ]
+    if [ $USERID -ne 0 ]
     then 
         echo -e "$2 ... $R FAILURE $N"
         exit 1
