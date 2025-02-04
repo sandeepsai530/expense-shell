f187bd1bbf354c1f21a0165019cccb5cf65f4c61
sai sandeep <sandeepsai530@gmail.com>
2025-02-04 17:17:24 +0530

    added backend.sh

--- backend.sh
+++ backend.sh
@@ -59,7 +59,8 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading backend"
 
-cd /app &>>$LOG_FILE_NAME
+cd /app 
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip backend"
